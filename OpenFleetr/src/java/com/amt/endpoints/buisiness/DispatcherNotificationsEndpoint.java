36e1330925dff87f79c04e2ca18d6598805fdcd3
Tareq Kirresh <kirreshtareq@gmail.com>
2018-05-05 15:44:01 +0300

    notification listing shg now works

--- OpenFleetr/src/java/com/amt/endpoints/buisiness/DispatcherNotificationsEndpoint.java
+++ OpenFleetr/src/java/com/amt/endpoints/buisiness/DispatcherNotificationsEndpoint.java
@@ -7,7 +7,6 @@ package com.amt.endpoints.buisiness;
 
 import com.amt.entities.auth.UserEntity;
 import com.amt.entities.buisiness.NotificationEntity;
-import com.amt.entities.management.DispatcherEntity;
 import com.amt.entities.management.GeographicalAreaEntity;
 import com.amt.common.data.GEOSql;
 import com.amt.entities.management.JurisdictionEntity;
@@ -29,15 +28,25 @@ public class DispatcherNotificationsEndpoint extends AuthorisedEndpoint {
     @Override
     public JSONObject doList(String string) throws AccessError {
         UserAccessControl.authOperation(UserEntity.class, string, 3);
+        
         JSONObject query1 = new JSONObject();
         query1.put("dispatcherId", UserAccessControl.fetchUserByToken(UserEntity.class, string).get("id"));
         JSONObject jurisdictions = Persistence.listByProperties(JurisdictionEntity.class, query1);
-        JSONObject query2 = new JSONObject();
+        
+        JSONObject orQuery = new JSONObject();
         for (Object key : jurisdictions.keySet()) {
             JSONObject jurisdiction = (JSONObject) jurisdictions.get(key);
-            query2.put("geographicalAreaId", jurisdiction.get("geographicalAreaId"));
+            orQuery.put("geographicalAreaId", jurisdiction.get("geographicalAreaId"));
         }
-        return Persistence.searchByProperties(NotificationEntity.class, query2);        
+        
+        JSONObject andQuery = new JSONObject();
+        andQuery.put("wasHandled",false);
+        
+        JSONObject query = new JSONObject();
+        query.put("or", orQuery);
+        query.put("and", andQuery);
+        
+        return Persistence.searchByProperties(NotificationEntity.class, query);        
     }
     
     @Override
@@ -48,7 +57,6 @@ public class DispatcherNotificationsEndpoint extends AuthorisedEndpoint {
         jsono.put("wasHandled", false);
         JSONObject query = new JSONObject();
         query.put("geographicalAreaId", GEOSql.liesWithinPolygon(GeographicalAreaEntity.class, jsono));
-        jsono.put("dispatcherId", Persistence.readByProperties(JurisdictionEntity.class, query).get("id"));
         return Persistence.create(NotificationEntity.class, jsono);
     }
     
@@ -70,7 +78,7 @@ public class DispatcherNotificationsEndpoint extends AuthorisedEndpoint {
             query2.put("wasHandled",false);
             return Persistence.listByProperties(NotificationEntity.class, query2);            
         } else {
-            throw new AccessError(ERROR_TYPE.USER_NOT_AUTHORISED);
+            throw new AccessError(ERROR_TYPE.USER_NOT_ALLOWED);
         }
     }
     
