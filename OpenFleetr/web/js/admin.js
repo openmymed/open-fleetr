80b2d3499653fe94a2affe3485e54ba6996a39d0
Tareq Kirresh <kirreshtareq@gmail.com>
2018-05-09 16:46:37 +0300

    added hospitals Management tab to administrator

--- OpenFleetr/web/js/admin.js
+++ OpenFleetr/web/js/admin.js
@@ -1,9 +1,12 @@
 var expanded = false;
-var map;
+var jurisdictionsMap;
+var hospitalsMap
 $(document).ready(main);
 
 function main(){
-    drawMap();
+    $('#hospitalsTab').click(drawHospitalsMap);
+    $('#jurisdictionsTab').click(drawJurisdictionsMap);
+
 }
 function showCheckboxes() {
     var checkboxes = document.getElementById("checkboxes");
@@ -16,12 +19,19 @@ function showCheckboxes() {
     }
 }
 
-function drawMap() {
-    console.log("no gis");
-    map = L.map('jurisdictionsMap',{zoomControl:false}).setView([31.7683, 35.2137], 13);
+function drawJurisdictionsMap() {
+    jurisdictionsMap = L.map('jurisdictionsMap',{zoomControl:false}).setView([31.7683, 35.2137], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(jurisdictionsMap);
+    
+}
+
+function drawHospitalsMap() {
+    hospitalsMap = L.map('hospitalsMap',{zoomControl:false}).setView([31.7683, 35.2137], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(hospitalsMap);
     
 }/* 
  * To change this license header, choose License Headers in Project Properties.
